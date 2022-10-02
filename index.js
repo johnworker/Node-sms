4640e0ce3da81238e4e82cc3b2d8d14c9e614360
johnworker <78348941+johnworker@users.noreply.github.com>
2022-10-03 06:52:06 +0800

    添加內容

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use('view engine', 'ejs')
+
+app.get('/',(req,res)=>{
+    res.send('Hello')
+})
+
 app.listen(3000, ()=>{
-    console.log("Server is listening on port 3000");
+    console.log("Server is listening on Port 3000");
 })
